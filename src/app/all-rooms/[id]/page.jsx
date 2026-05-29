26f31bf75d40c29f0028895e74940c3f1b2d4908
MaksumulEmon <maksumulemon300@gmail.com>
2026-05-29 20:27:42 +0600

    Fixed & Polish Issus

--- src/app/all-rooms/[id]/page.jsx
+++ src/app/all-rooms/[id]/page.jsx
@@ -127,7 +127,7 @@ const RoomDetailsBookPage = async ({ params }) => {
                                             <circle cx="12" cy="12" r="10" />
                                             <path d="M12 6v6l4 2" />
                                         </svg>
-                                        {room.totalBookings}
+                                        {room.bookingsCount || 0} : Total Bookings
                                     </div>
                                 </div>
 
